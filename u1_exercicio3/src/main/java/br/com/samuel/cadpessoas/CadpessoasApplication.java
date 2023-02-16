5fda362e5c9a593fa51c9676f828d506cbcc8a32
Samuelaraujo10 <samuelaraujo71@gmail.com>
2023-02-16 19:09:14 -0300

    Primeiras configurações

---
+++ u1_exercicio3/src/main/java/br/com/samuel/cadpessoas/CadpessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/src/test/java/br/com/samuel/cadpessoas/CadpessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuel.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
