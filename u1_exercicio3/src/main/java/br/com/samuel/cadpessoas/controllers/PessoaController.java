d5df508daa3144736c811615bc76aa95f3903bb8
Samuelaraujo10 <samuelaraujo71@gmail.com>
2023-03-04 17:15:25 -0300

    update cad_pessoas PessoaController.java

--- u1_exercicio3/src/main/java/br/com/samuel/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/src/main/java/br/com/samuel/cadpessoas/controllers/PessoaController.java
@@ -1,15 +1,38 @@
 package br.com.samuel.cadpessoas.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.samuel.cadpessoas.model.Pessoa;
+import br.com.samuel.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
-  // raiz do site localhost:8080
+  @Autowired
+  PessoaRepository pessoaRepo;
+
+  public PessoaController(PessoaRepository pessoaRepo) {
+    this.pessoaRepo = pessoaRepo;
+  }
+
+  // raiz do site localhost:8080/
   @GetMapping
   public String index() {
     return "index.html";
   }
+
+  @GetMapping("/listarPessoas")
+  public ModelAndView listarPessas() {
+    List<Pessoa> todasAsPessoas = pessoaRepo.findAll();
+    ModelAndView modelAndView = new ModelAndView("ListarPessoas");
+    modelAndView.addObject("todasAsPessoas", todasAsPessoas);
+    return modelAndView;
+  }
+
 }
--- u1_exercicio3/src/main/java/br/com/samuel/cadpessoas/repositories/PessoaRepository.java
+++ u1_exercicio3/src/main/java/br/com/samuel/cadpessoas/repositories/PessoaRepository.java
@@ -1,5 +1,12 @@
 package br.com.samuel.cadpessoas.repositories;
 
-public class PessoaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.samuel.cadpessoas.model.Pessoa;
+
+@Repository
+public interface PessoaRepository
+    extends JpaRepository<Pessoa, Long> {
 
 }
